da6a888ee36db6ac4f088fa6d9d59159fa814050
BenJenkyn <jenkynb@gmail.com>
2026-02-20 12:29:35 -0500

    bubbles trap enemies

--- scenes/entities/player/Player.gd
+++ scenes/entities/player/Player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+var bubble_scene = preload("res://scenes/entities/projectiles/Bubble.tscn")
+var last_direction := 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,7 +21,18 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		last_direction = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_just_pressed("fire"):
+		shoot(last_direction)
 
 	move_and_slide()
+
+func shoot(direction):
+	var bubble = bubble_scene.instantiate()
+	bubble.position.y = position.y
+	bubble.position.x = position.x + direction * 50
+	bubble.dir = direction   # -1 for left, 1 for right
+	get_parent().add_child(bubble)
